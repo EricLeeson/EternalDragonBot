aa536bc81dafc36d47878ff39837b3f39cb01d09
Eric Leeson <ericleesoncs@gmail.com>
2023-10-10 00:34:45 -0700

    Added logging functionality

--- events/guildScheduledEventUserRemove.js
+++ events/guildScheduledEventUserRemove.js
@@ -1,13 +1,20 @@
-const { Events } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Events, GuildScheduledEvent, GuildScheduledEventManager } = require('discord.js');
 
 module.exports = {
     name : Events.GuildScheduledEventUserRemove,
     once: false,
     async execute(scheduledEvent, user) {
-        const id = scheduledEvent.id;
         if (scheduledEvent.description.includes('Practice')) {
             try {
-                await console.log('-1');
+                const client = scheduledEvent.client;
+                const logChannel = await client.channels.cache.get(process.env.LOGS_CHANNEL_ID);
+                const guild = await client.guilds.cache.get(process.env.GUILD_ID);
+                const member = await guild.members.cache.get(user.id);
+                const practiceType = (scheduledEvent.description.endsWith('Water Practice!')) ? 'Water' : 'Land';
+                await logChannel.send(`${member.nickname} unsigned up from ${practiceType} Practice. (${scheduledEvent.id})`);
             } catch (error) {
                 console.error(error);
             }
--- takeAttendance.js
+++ takeAttendance.js
@@ -1,10 +1,11 @@
 const googleSheets = require('./googleSheets');
 
 async function execute(scheduledEvent) {
+    const logChannel = await scheduledEvent.client.channels.cache.get(process.env.LOGS_CHANNEL_ID);
     const subs = await scheduledEvent.fetchSubscribers({withMember : true});
     const nicknames = getNicknames(subs);
     googleSheets.takeAttendance(nicknames);
-    console.log('Took attendance.');
+    await logChannel.send(`Took attendance. There are ${nicknames.length} people signed up.`);
 }
 
 module.exports = {
